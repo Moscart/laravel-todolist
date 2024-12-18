1ff3a7105555d82eb6569b33bacf829dac785f6f
Moscart <danieltheo.73@gmail.com>
2024-12-18 23:00:08 +0700

    feat: create login action

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -18,4 +18,28 @@ class UserControllerTest extends TestCase
         $this->get('/login')
             ->assertSeeText('Login');
     }
+
+    public function testLoginSuccess()
+    {
+        $this->post('/login', [
+            'user' => 'daniel',
+            'password' => 'rahasia'
+        ])->assertRedirect('/')
+            ->assertSessionHas('user', 'daniel');
+    }
+
+    public function testLoginValidationError()
+    {
+        $this->post('/login', [])
+            ->assertSeeText("User or password is required");
+    }
+
+    public function testLoginFailed()
+    {
+        $this->post('/login', [
+            'user' => 'wrong',
+            'password' => 'wrong'
+        ])
+            ->assertSeeText("User or password is wrong");
+    }
 }
