633c3d3ee8567c9d04f3ac118a5a89ab830b87f5
Moscart <danieltheo.73@gmail.com>
2024-12-18 22:49:35 +0700

    feat: create login page

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -13,10 +13,9 @@ class UserControllerTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testLogin()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->get('/login')
+            ->assertSeeText('Login');
     }
 }
